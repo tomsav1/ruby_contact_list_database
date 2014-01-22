069bb7d5d1e82280225ad8d2260551deaf0d83b3
Tom Sav <tomsav2010@gmail.com>
2014-01-22 23:53:26 +0000

    on validation

--- application.rb
+++ application.rb
@@ -21,18 +21,21 @@ class Application
       if input == "new"
         new_contact_creation
       elsif input == "list"
-        Contact.all.each do |thing|
-          print thing
+        Contact.all.each do |record|
+          print record.first_name + " "
+          print record.last_name + " "
+          puts record.email 
         end 
       elsif input == "show"
         #puts Contact.all
         #Contact.to_s
         puts "Which id?"
-        id_selected = get_input
-.to_i
-        contact = Contact.get(id_selected)
+        id_selected = get_input.to_i
+        current_contact = Contact.find_by id: id_selected
+        #contact = Contact.get(id_selected)
         puts "Contact details below:"
-        puts contact # automatic conversion to string
+        print current_contact.id.to_s + " " + current_contact.first_name + " " 
+        puts current_contact.last_name + " " + current_contact.email
         puts "Do you want to edit this contact? y or n?"
         if get_input == "y"
           puts "What do you want to edit? fname, lname, or email?"
@@ -42,20 +45,57 @@ class Application
             when "fname"
               puts "New first name?"
               new_first_name = get_input
-
-              contact.first_name  = new_first_name
+              current_contact.update(:first_name => new_first_name)
             when "lname"
               puts "New last name?"
               new_last_name = get_input
-
-              contact.last_name  = new_last_name
+              current_contact.update(:last_name => new_last_name)
             when "email"
               puts "New email?"
               new_email = get_input
-
-              contact.email = new_email
+              current_contact.update(:email => new_email)
           end
+
         end
+      elsif input == "delete"
+        puts "Which id?"
+        id_selected = get_input.to_i
+        Contact.delete(id_selected)
+      elsif input == "importance"
+        Contact.where(importance: [1,2,3,4,5]).order("importance").each do |record|
+        #Contact.all.each do |record|
+          print record.importance.to_s + " "
+          print record.first_name + " "
+          print record.last_name + " "
+          puts record.email 
+        end
+      elsif input == "search"
+        puts "What do you want to search by? fname, lname, or email?"
+        search_option = get_input
+        case search_option
+          when "fname"
+            puts "Enter the first name."
+            search_first_name = get_input
+            found_contact = Contact.find_by first_name: search_first_name
+            puts "Contact details below:"
+            print found_contact.id.to_s + " " + found_contact.first_name + " " 
+            puts found_contact.last_name + " " + found_contact.email
+          when "lname"
+            puts "Enter the last name."
+            search_last_name = get_input
+            found_contact = Contact.find_by last_name: search_last_name
+            puts "Contact details below:"
+            print found_contact.id.to_s + " " + found_contact.first_name + " " 
+            puts found_contact.last_name + " " + found_contact.email
+          when "email"
+            puts "Enter the email."
+            search_email = get_input
+            found_contact = Contact.find_by email: search_email
+            puts "Contact details below:"
+            print found_contact.id.to_s + " " + found_contact.first_name + " " 
+            puts found_contact.last_name + " " + found_contact.email
+        end
+        
       end
     end
 
@@ -66,10 +106,13 @@ class Application
   # Prints the main menu only
   def show_main_menu
     puts "Welcome to the app. What's next?"
-    puts " new      - Create a new contact"
-    puts " list     - List all contacts"
-    puts " show :id - Display contact details"
-    puts " quit     - Quit the application"
+    puts " new        - Create a new contact"
+    puts " list       - List all contacts"
+    puts " show :id   - Display contact details"
+    puts " delete: id - Deletes contact record"
+    puts " importance - sorted list by importance"
+    puts " search     - search for contact"
+    puts " quit       - Quit the application"
     print "> "
   end
 
@@ -83,16 +126,18 @@ class Application
     puts "What is the email?"
     email = get_input
 
-    [first_name, last_name, email]
+    puts "Level of importance? (1-5)"
+    importance = get_input
+
+    [first_name, last_name, email, importance]
   end
 
   def new_contact_creation
-    first_name, last_name, email = get_contact_information
-    Contact.create(first_name, last_name, email)
+    first_name, last_name, email, importance = get_contact_information
+    Contact.create(first_name: first_name, last_name: last_name, email: email, importance: importance)
   end
 
   def create_stub_data
-
   end
 
   def get_input
--- contact.rb
+++ contact.rb
@@ -1,37 +1,46 @@
 require 'active_record'
 class Contact < ActiveRecord::Base
 
-  @@instances = []
+  #validates :importance, inclusion: { in: %w(nil 1 2 3 4 5), message: "%{value} is not a valid importance level" }
+  #validates :email, uniqueness: true
+
+  #@@instances = []
 
   # Contact.having_email?(email)
   # class method
   def self.email_taken?(email)
-    @@instances.detect { |contact| contact.email == email }
+    #@@instances.detect { |contact| contact.email == email }
+    Contact.all.detect { |contact| contact.email == email }
   end
 
   # Contact.create
-  def self.create(first, last, email)
-    unless email_taken?(email)
-      contact = new(first, last, email)
-      @@instances << contact
-    end
-  end
+  #def self.create(first, last, email)
+  #   unless email_taken?(email)
+  #     contact = new(first, last, email)
+  #     #@@instances << contact
+  #     Contact << contact
+  #   end
+  # end
 
-  def self.get(id)
-          @@instances[id.to_i]
-  end
+  # def self.get(id)
+  #         #@@instances[id.to_i]
+  #         Contact.find_by(id: id.to_i)
+  # end
 
-  def self.all
-          @@instances
-  end
+  # def self.all
+  #         #@@instances
+  #         Contact.all
+  # end
 
-  def self.count
-          @@instances.size
-  end
+  # def self.count
+  #         #@@instances.size
+  #         Contact.size
+  # end
 
-  def self.instances
-          @@instances
-  end
+  # def self.instances
+  #         #@@instances
+  #         Contact
+  # end
 
   def to_s
     "id: #{@id} #{@first_name} #{@last_name} (#{@email})\n"
